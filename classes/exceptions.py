2f8479e54d8bb2624acd324dfd83419b4cbe80ee
Michele Costa Christianson <michpimentel22@gmail.com>
2023-12-18 17:14:25 -0700

    class

---
+++ classes/exceptions.py
@@ -0,0 +1,15 @@
+"""
+Try and except keywords to handles user input error and not get a error from python
+"""
+ 
+# try: # code that could raise exception
+#     value = int(input('Enter an interger: '))
+#     print('The inverse of', value, 'is', 1/value)
+
+# except ValueError: # named the exception
+#     # code that handles the exception / errors
+#     print('You did not provide a number') # user input was not a interger
+
+# except
+
+
